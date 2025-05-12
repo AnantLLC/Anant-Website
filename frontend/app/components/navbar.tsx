37ddace60b8c9622c95356af710159d843d9b2a7
birongliu <birongliu2003@gmail.com>
2025-05-12 13:41:24 -0400

    Increase logo size in Navbar for better visibility

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -91,7 +91,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <Link href="/" className="flex items-center">
-            <div className="h-24 w-24 relative">
+            <div className="h-32 w-32 relative">
               <Image
                 src={logo}
                 alt="Anant Logo"
