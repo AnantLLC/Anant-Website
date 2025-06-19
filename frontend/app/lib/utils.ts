74d4ad8c691f4eee51a06b044485939575cfbfe7
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-06-19 05:37:29 -0400

    Feat: implemented smooth scroll navigation across all links in navbar, footer and hero

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export  function handleScrollToSection(e: React.MouseEvent<HTMLAnchorElement>, href: string) {
+    if (!href.startsWith("#")) return
+
+    e.preventDefault();
+    const element = document.querySelector(href);
+    if (element) {
+        const navbarHeight = 60;
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - navbarHeight;
+
+        window.scrollTo({top: offsetPosition, behavior: "smooth" });
+    }
+  }
