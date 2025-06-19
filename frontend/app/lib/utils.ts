e0976cf232b69e7ecddd2caddca92e49f44090bc
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-06-19 16:23:17 -0400

    changed dynamic height calculation to using top margin

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -5,17 +5,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export  function handleScrollToSection(e: React.MouseEvent<HTMLAnchorElement>, href: string) {
+export function handleScrollToSection(e: React.MouseEvent<HTMLAnchorElement>, href: string) {
     if (!href.startsWith("#")) return
 
     e.preventDefault();
     const element = document.querySelector(href);
     if (element) {
-        const navbar = document.querySelector("nav");
-        const navbarHeight = navbar ? navbar.getBoundingClientRect().height : 0;
-        const elementPosition = element.getBoundingClientRect().top;
-        const offsetPosition = elementPosition + window.pageYOffset - navbarHeight;
-
-        window.scrollTo({top: offsetPosition, behavior: "smooth" });
+        window.scrollTo({
+            top: element.getBoundingClientRect().top + window.pageYOffset - 30,
+            behavior: "smooth"
+        });
     }
   }
