b129e5fd5f5d989d3678c32b73ae85c878e42c63
Yajat Parmar <yajat4life@gmail.com>
2025-07-01 00:17:03 -0500

    V2 Microsoft Clarity Install

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,11 +5,30 @@ import React from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "r1avrpdi0c");
+              
+              // Initialize Clarity
+              window.clarity && window.clarity('start', 'r1avrpdi0c');
+            `,
+          }}
+        />
+      </head>
       <body> 
-      <Navbar />
-      {children}
-      <Footer />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
